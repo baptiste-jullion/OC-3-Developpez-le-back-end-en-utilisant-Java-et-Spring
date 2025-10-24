32a47753fef5227e0d5f56a428a983b37c3ac838
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-10-24 09:55:00 +0200

    feat: Add HealthcheckController with healthcheck endpoint

---
+++ api/src/main/java/com/chatop/api/controller/HealthcheckController.java
@@ -0,0 +1,14 @@
+package com.chatop.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthcheckController {
+
+    @GetMapping("/api/healthcheck")
+    public String healthcheck() {
+        return "API is up!";
+    }
+
+}
