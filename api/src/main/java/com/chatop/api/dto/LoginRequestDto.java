bac42225ad411c02369ebbcae7a4c5a5687e1a55
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-10-30 21:58:08 +0100

    feat: Simplify DTOs by replacing @Getter and @Setter with @Data annotations

--- api/src/main/java/com/chatop/api/dto/LoginRequestDto.java
+++ api/src/main/java/com/chatop/api/dto/LoginRequestDto.java
@@ -1,10 +1,8 @@
 package com.chatop.api.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class LoginRequestDto {
     private String login;
     private String password;
