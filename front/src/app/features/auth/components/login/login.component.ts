9101cb4df86ef66c0c8218074585b4bb9006f38c
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-11-06 21:49:21 +0100

    refactor: Update login form to use 'login' instead of 'email' and improve code formatting

--- front/src/app/features/auth/components/login/login.component.ts
+++ front/src/app/features/auth/components/login/login.component.ts
@@ -1,30 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { User } from 'src/app/interfaces/user.interface';
-import { SessionService } from 'src/app/services/session.service';
-import { AuthSuccess } from '../../interfaces/authSuccess.interface';
-import { LoginRequest } from '../../interfaces/loginRequest.interface'; 
-import { AuthService } from '../../services/auth.service';
+import {Component} from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {User} from 'src/app/interfaces/user.interface';
+import {SessionService} from 'src/app/services/session.service';
+import {AuthSuccess} from '../../interfaces/authSuccess.interface';
+import {LoginRequest} from '../../interfaces/loginRequest.interface';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent  {
+export class LoginComponent {
   public hide = true;
   public onError = false;
 
   public form = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
+    login: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.min(3)]]
   });
 
-  constructor(private authService: AuthService, 
-    private fb: FormBuilder, 
-    private router: Router,
-    private sessionService: SessionService) { }
+  constructor(private authService: AuthService,
+              private fb: FormBuilder,
+              private router: Router,
+              private sessionService: SessionService) {
+  }
 
   public submit(): void {
     const loginRequest = this.form.value as LoginRequest;
