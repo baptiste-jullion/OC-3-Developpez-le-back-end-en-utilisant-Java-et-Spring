7042774455c68672c065b6c26cc54931abc36ad8
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-11-07 11:22:45 +0100

    feat: Update RentalMapper to handle picture URLs and modify rental creation and update methods

--- api/src/main/java/com/chatop/api/mapper/RentalMapper.java
+++ api/src/main/java/com/chatop/api/mapper/RentalMapper.java
@@ -5,6 +5,7 @@ import com.chatop.api.dto.rentals.RentalDto;
 import com.chatop.api.dto.rentals.RentalUpdateRequestDto;
 import com.chatop.api.model.Rental;
 import com.chatop.api.model.User;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 public class RentalMapper {
     public static RentalDto toDto(Rental rental) {
@@ -14,7 +15,13 @@ public class RentalMapper {
         dto.setName(rental.getName());
         dto.setSurface(rental.getSurface());
         dto.setPrice(rental.getPrice());
-        dto.setPicture(rental.getPicture());
+        if (rental.getPicture() != null && !rental.getPicture().isEmpty()) {
+            String filePath = "/uploads/" + rental.getPicture();
+            String fullUrl = ServletUriComponentsBuilder.fromCurrentContextPath().path(filePath).toUriString();
+            dto.setPicture(fullUrl);
+        } else {
+            dto.setPicture(null);
+        }
         dto.setDescription(rental.getDescription());
         dto.setOwnerId(rental.getOwner() != null ? rental.getOwner().getId() : null);
         dto.setCreatedAt(rental.getCreatedAt());
@@ -22,23 +29,23 @@ public class RentalMapper {
         return dto;
     }
 
-    public static Rental fromCreateDto(RentalCreateRequestDto dto, User owner) {
+    public static Rental fromCreateDto(RentalCreateRequestDto dto, User owner, String picturePath) {
         if (dto == null) return null;
         Rental rental = new Rental();
         rental.setName(dto.getName());
         rental.setSurface(dto.getSurface());
         rental.setPrice(dto.getPrice());
-        rental.setPicture(dto.getPicture());
+        rental.setPicture(picturePath);
         rental.setDescription(dto.getDescription());
         rental.setOwner(owner);
         return rental;
     }
 
-    public static void updateRentalFromDto(Rental rental, RentalUpdateRequestDto dto) {
+    public static void updateRentalFromDto(Rental rental, RentalUpdateRequestDto dto, String picturePath) {
         if (dto.getName() != null) rental.setName(dto.getName());
         if (dto.getSurface() != null) rental.setSurface(dto.getSurface());
         if (dto.getPrice() != null) rental.setPrice(dto.getPrice());
-        if (dto.getPicture() != null) rental.setPicture(dto.getPicture());
+        if (picturePath != null) rental.setPicture(picturePath);
         if (dto.getDescription() != null) rental.setDescription(dto.getDescription());
     }
 }
