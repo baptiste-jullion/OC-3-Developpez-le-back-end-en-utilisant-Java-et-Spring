bac1ba62024a75f5c4d5b8f9ed70edaac879e9a5
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-11-13 23:23:43 +0100

    feat: Update SecurityConfig to permit access to Swagger UI endpoints and add springdoc-openapi dependency

--- api/src/main/java/com/chatop/api/config/SecurityConfig.java
+++ api/src/main/java/com/chatop/api/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.chatop.api.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,9 +9,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import com.chatop.api.config.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -21,14 +20,22 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-        .csrf(AbstractHttpConfigurer::disable)
-        .authorizeHttpRequests(authorize -> authorize
-            .requestMatchers("/api/healthcheck", "/api/auth/**", "/error", "/uploads/**").permitAll()
-            .anyRequest().authenticated()
-        )
-        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-    return http.build();
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/api/healthcheck",
+                                "/api/auth/**",
+                                "/error",
+                                "/uploads/**",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 
     @Bean
